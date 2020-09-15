008ce8b5bb6b807951a11263be915c31f2c9d0ce
ankitapuri <puriankita30@gmail.com>
2020-09-16 01:10:55 +0530

    added file

---
+++ create_display_circularLL.cpp
@@ -0,0 +1,70 @@
+#include<iostream>
+#include<cstddef>
+using namespace std;
+class node
+{
+public:
+	int data;
+	
+	node *next=NULL;
+    node*start=NULL;   //declaring the start pointer as NULL as it not pointing anywhere currently
+
+void create(int k)   //function begins for creation of linked list
+{
+	node *ptr,*ptr1;
+	
+  for(int i=0;i<k;i++)
+  {
+	  ptr=new node;
+	  cin>>ptr->data;
+	  ptr->next=NULL;
+	  if(start==NULL)
+	   {
+	   start=ptr;
+	   }
+	   else
+	   {
+	   ptr1=start;
+	   while (ptr1->next!=NULL)
+		{
+		   ptr1=ptr1->next;
+	   	}
+	   ptr1->next=ptr;
+	   		   
+	   } 
+  }
+   ptr1=start;
+	   while (ptr1->next!=NULL)
+		{
+		   ptr1=ptr1->next;
+	   	}
+	   	ptr1->next=start;
+  
+  
+}
+ void display()            //function begins for display of linked list
+ {
+	 node *temp=start->next;
+	 cout<<start->data<<" ";
+	 while(temp!=start)
+	 {
+	 	
+		cout<<temp->data<<" ";
+		 temp=temp->next;
+	 }
+	 
+ }
+
+
+
+ };
+int main()
+{
+	node N;
+	int n;
+	cout<<"enter the number of elements";
+	cin>>n;
+	N.create(n);
+	N.display();
+	
+}
